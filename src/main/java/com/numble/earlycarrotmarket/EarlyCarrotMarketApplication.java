a7aa0522b85ee56e4d1cb2c1e295ab45c0f30e39
boompatron <grapeflavoredapple@gmail.com>
2023-06-04 13:54:17 +0900

    Initial commit

---
+++ src/main/java/com/numble/earlycarrotmarket/EarlyCarrotMarketApplication.java
@@ -0,0 +1,13 @@
+package com.numble.earlycarrotmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyCarrotMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlyCarrotMarketApplication.class, args);
+	}
+
+}
